9089343cf4d8567ec9ec305770fac2f84f0cc7fd
Joey Anuff <joeyanuff@gmail.com>
2023-05-16 18:07:28 -0700

    TagCloud text size

--- components/TagCloud.tsx
+++ components/TagCloud.tsx
@@ -18,7 +18,7 @@ const TagCloud: FC<TagCloudProps> = ({ tags, onTagClick }) => (
     <div className="tag-cloud flex justify-center flex-wrap gap-2 p-4 max-w-lg mx-auto my-1">
     {tags.map((tag, index) => (     
         <button 
-          className="tag uppercase font-semibold px-2 py-1 rounded bg-gray-200 text-gray-700 text-sm hover:bg-gray-300 transition duration-200 ease-in-out"
+          className="tag uppercase font-semibold px-2 py-1 rounded bg-gray-200 text-gray-700 text-xs hover:bg-gray-300 transition duration-200 ease-in-out"
           key={index} 
           onClick={(event: MouseEvent<HTMLButtonElement>) => onTagClick(tag)}>
           {tag}
