e2608edcbf3d08fa75d3187e1fee3c1b0561f8d1
Joey Anuff <joeyanuff@gmail.com>
2023-05-18 20:45:08 -0700

    Books component inside div

--- pages/index.tsx
+++ pages/index.tsx
@@ -194,8 +194,9 @@ export default function Home() {
 
       <div className="flex flex-col h-screen">
         <Navbar />
-        <Book />
+
         <div className="flex-1 overflow-auto">
+        <Book />
           <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center px-5">
             <button
               className="mt-4 flex cursor-pointer items-center space-x-2 rounded-full border border-zinc-600 px-3 py-1 text-sm hover:opacity-50 hidden"
